4c85153585ec7c2718697aef096d796fab3d9eb6
Gohér László <goher.laci@gmail.com>
2024-05-11 19:36:52 +0200

    setup Zod

--- src/task.ts
+++ src/task.ts
@@ -1,7 +1,12 @@
-export interface Task {
-    name: String,
-    description: String,
-}
+import {z} from "zod";
+
+
+const taskSchema = z.object({
+    name: z.string(),
+    description: z.string(),
+});
+
+export type Task = z.infer<typeof taskSchema>;
 
 export function do_stuff(t: Task) {
     t.description = t.name;
