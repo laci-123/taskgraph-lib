bd610232d8214db27dac61b5bdc139fba6f11d57
Gohér László <goher.laci@gmail.com>
2024-05-12 19:42:04 +0200

    move small types to utils module

--- src/task.ts
+++ src/task.ts
@@ -1,45 +1,4 @@
-/**
- * Used to prevent `TypeDoc` from resolving type aliases.
- * See [this issue](https://github.com/TypeStrong/typedoc/issues/1502#issuecomment-775645305)
- * for more details.
- */
-export type dummy_type = {_never_used?: never};
-
-
-/**
- * Integer type.
- */
-export type Integer = number & {_never_used: void};
-
-/**
- * Converts a number to an {@link Integer}.
- *
- * Throws error if `x` is not an integer.
- */
-export function number_to_int(x: number): Integer {
-    if(Number.isInteger(x)) {
-        return x as Integer;
-    }
-    else {
-        throw new Error(`${x} is not an integer`);
-    }
-}
-
-
-/**
- * Milliseconds since the [Unix Epoch](https://en.wikipedia.org/wiki/Unix_time).
- * Easy to convert to/from the JavaScript `Date` type.
- * 
- * Uses `Infinity` to represent a point in time that is infinitely far in the future.
- * Uses `Negative Infinity` to represent a point in time that is infinitely far in the past.
- */
-export type MillisecondsSinceEpoch = number & dummy_type;
-
-
-/**
- * A time span in milliseconds.
- */
-export type Milliseconds = number & dummy_type;
+import {Milliseconds, MillisecondsSinceEpoch, Integer} from "./utils";
 
 
 /**
